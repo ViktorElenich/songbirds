13bae4d24d6a91f91f51ce81d6efca70dfdc52cc
Viktor <velenic@mail.ru>
2022-11-19 20:37:45 +0300

    feat: add show result function

--- songbirds/src/pages/game/game-page.js
+++ songbirds/src/pages/game/game-page.js
@@ -1,5 +1,5 @@
 import { Page } from "../../js/templates/pages";
-import {customCreateElement, getRandomNum, shuffle} from "../../js/utils/utils";
+import {customCreateElement, getRandomNum, setToLocalStorage, shuffle} from "../../js/utils/utils";
 import { GameNavigation } from "../../helpers";
 import { ANSWERS_COUNT } from "../../helpers/const";
 import birdsData from "../../data/data";
@@ -17,7 +17,8 @@ export class GamePage extends Page {
     this.count = 5;
     this.isCorrect = false;
     this.audio = new Audio();
-    this.song = new Audio;
+    this.audioInfo = new Audio();
+    this.song = new Audio();
     this.currentClick = null;
   }
 
@@ -63,7 +64,7 @@ export class GamePage extends Page {
       </div>
       <div class="player-slider__container">
         <i class="fa fa-volume-down"></i>
-        <input type="range" min="0" max="100" value="99" class="volume_slider">
+        <input type="range" min="0" max="100" value="100" class="volume_slider">
         <i class="fa fa-volume-up"></i>
       </div>
     `;
@@ -86,43 +87,51 @@ export class GamePage extends Page {
         <button class="answer-btn answer"></button>
         <button class="answer-btn answer"></button>
       </div>
-    `
+    `;
 
-    gameAnswerContainer.append(answerBlock, answerInfo)
+    answerInfo.innerHTML = `
+      <p style="text-align: center; font-size: 2vw;">Послушайте плеер. Выберите птицу из списка</p>
+    `;
 
-    gameContainer.append(gameContainerNav, gameContainerQuestion, gameAnswerContainer);
+    gameAnswerContainer.append(answerBlock, answerInfo);
+
+    const nextLevelBtn = customCreateElement('button', 'game-page__container-nextLevel-btn');
+    nextLevelBtn.textContent = 'Next Level';
+
+    gameContainer.append(gameContainerNav, gameContainerQuestion, gameAnswerContainer, nextLevelBtn);
     gamePage.append(gameContainer);
     this.container.append(gamePage);
   }
 
   choiceAnswer(event) {
-    const allAnswer = document.querySelectorAll('.answer-btn');
-    for (let i = 0; i < allAnswer.length; i++) {
-      if (allAnswer[i] === event.target) this.currentClick = i;
+    for (let i = 0; i < this.answersEl.length; i++) {
+      if (this.answersEl[i].id === event.target.id) this.currentClick = event.target.id;
     }
-    console.log(this.currentClick)
   }
 
   generateAnswers() {
-    const answers = [];
+    const answer = [];
     this.currentQuestion = getRandomNum(this.level);
 
     const rightAnswer = birdsData[this.level][this.currentQuestion].name;
     this.audio.src = birdsData[this.level][this.currentQuestion].audio;
+    const id = birdsData[this.level][this.currentQuestion];
+    console.log(rightAnswer)
 
     this.audioPlayer(this.mainPlayer, birdsData[this.level][this.currentQuestion].audio, this.audio);
     this.rightAnswer = rightAnswer;
-    answers.push(rightAnswer);
+    answer.push(id);
 
-    while (answers.length < ANSWERS_COUNT) {
+    while (answer.length < ANSWERS_COUNT) {
       const randomAnswer = birdsData[this.level][Math.floor(Math.random() * birdsData[this.level].length)];
 
-      if (!answers.includes(randomAnswer.name)) {
-        answers.push(randomAnswer.name);
+      if (!answer.includes(randomAnswer)) {
+        answer.push(randomAnswer)
       }
     }
-    shuffle(answers);
-    this.answersEl.forEach((el, idx) => (el.textContent = answers[idx]));
+    shuffle(answer);
+    this.answersEl.forEach((el, idx) => (el.textContent = answer[idx].name));
+    this.answersEl.forEach((el, idx) => (el.id = answer[idx].id));
 
     this.answersEl.forEach((el) => {
       el.classList.remove('correct');
@@ -131,11 +140,18 @@ export class GamePage extends Page {
   }
 
   findElements() {
+    this.gamePage = document.querySelector('.game-page__container');
     this.birdImage = document.querySelector('.bird-image');
     this.birdText = document.querySelectorAll('.block-item')[0];
     this.answersEl = document.querySelectorAll('.answer');
     this.answerContainer = document.querySelector('.answer-container');
     this.mainPlayer = document.querySelector('.audio-player');
+    this.answerInfo = document.querySelector('.answer-info');
+    this.nextLevelBtn = document.querySelector('.game-page__container-nextLevel-btn');
+    this.pagination = document.querySelectorAll('.navigation-item');
+    this.questionText = document.querySelector('.block-item');
+    this.playBtn = document.querySelector('.playpause-track');
+    this.scoreCont = document.querySelector('.header-container__score');
   }
 
   listeners() {
@@ -143,21 +159,25 @@ export class GamePage extends Page {
     quizAnswerContainer.addEventListener('click', (event) => {
       const { target } = event;
       const isAnswer = target.classList.contains('answer');
-
       if (isAnswer) {
         this.answer(target);
       }
-    })
-
+      this.answersEl.forEach((el) => {
+        if (el.classList.contains('correct')) {
+          target.classList.remove('incorrect')
+        }
+      })
+    });
     this.answerContainer.addEventListener('click', (event) => {
       this.choiceAnswer(event);
     });
-
+    this.nextLevelBtn.addEventListener('click', () => {
+      this.nextLevel()
+    });
   }
 
   async answer(answer) {
     let answerText;
-    const scoreCont = document.querySelector('.header-container__score');
 
     answerText = answer === 'timeout' ? 'timeout' : answer.innerHTML;
     if (answerText === this.rightAnswer) {
@@ -170,6 +190,37 @@ export class GamePage extends Page {
       this.score += this.count;
       this.birdImage.src = birdsData[this.level][this.currentQuestion].image;
       this.birdText.textContent = birdsData[this.level][this.currentQuestion].name;
+      this.answerInfo.innerHTML = `
+        <div class="answer-info__container">
+          <img class="bird-image" src="${birdsData[this.level][this.currentQuestion].image}" alt="bird">
+          <ul class="answer-info__container-block">
+            <li class="block-item">${birdsData[this.level][this.currentQuestion].name}</li>
+            <li class="block-item">${birdsData[this.level][this.currentQuestion].species}</li>
+            <li class="block-item">
+              <div class="audio-player">
+                <div class="player-slider__container">
+                  <div class="playpause-track">
+                    <i class="fa fa-play-circle fa-5x"></i>
+                  </div>
+                  <div class="current-time">00:00</div>
+                  <input type="range" min="1" max="100" value="0" class="seek_slider">
+                  <div class="total-duration">00:00</div>
+                </div>
+                <div class="player-slider__container">
+                  <i class="fa fa-volume-down"></i>
+                  <input type="range" min="0" max="100" value="100" class="volume_slider">
+                  <i class="fa fa-volume-up"></i>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <p>${birdsData[this.level][this.currentQuestion].description}</p>
+      `;
+      this.nextLevelBtn.classList.add('btn-next');
+      this.audioPlayer(this.answerInfo, birdsData[this.level][this.currentQuestion].audio, this.audioInfo);
+      this.audioInfo.pause();
+      this.audioInfo.currentTime = 0;
     } else if (answerText === 'timeout') {
       this.isCorrect = false;
     } else {
@@ -178,8 +229,39 @@ export class GamePage extends Page {
       this.count--;
       this.song.src = `./assets/fail.mp3`;
       await this.song.play();
+      this.audioInfo.pause();
+      this.audioInfo.currentTime = 0;
+      this.answerInfo.innerHTML = `
+        <div class="answer-info__container">
+          <img class="bird-image" src="${birdsData[this.level][this.currentClick - 1].image}" alt="bird">
+          <ul class="answer-info__container-block">
+            <li class="block-item">${birdsData[this.level][this.currentClick - 1].name}</li>
+            <li class="block-item">${birdsData[this.level][this.currentClick - 1].species}</li>
+            <li class="block-item">
+              <div class="audio-player">
+                <div class="player-slider__container">
+                  <div class="playpause-track">
+                    <i class="fa fa-play-circle fa-5x"></i>
+                  </div>
+                  <div class="current-time">00:00</div>
+                  <input type="range" min="1" max="100" value="0" class="seek_slider">
+                  <div class="total-duration">00:00</div>
+                </div>
+                <div class="player-slider__container">
+                  <i class="fa fa-volume-down"></i>
+                  <input type="range" min="0" max="100" value="100" class="volume_slider">
+                  <i class="fa fa-volume-up"></i>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <p>${birdsData[this.level][this.currentClick - 1].description}</p>
+      `;
+      this.audioPlayer(this.answerInfo, birdsData[this.level][this.currentClick - 1].audio, this.audioInfo);
     }
-    scoreCont.innerHTML = `Score: ${this.score}`;
+
+    this.scoreCont.innerHTML = `Score: ${this.score}`;
   }
 
   audioPlayer(block, song, audio) {
@@ -252,6 +334,103 @@ export class GamePage extends Page {
     });
   }
 
+  nextLevel() {
+    if (this.level === 5) {
+      if (this.score === 30){
+        this.showResult();
+      } else {
+        this.showModalResult()
+      }
+    }
+    if (this.nextLevelBtn.classList.contains('btn-next')) {
+      this.level++;
+      this.count = 5;
+      this.nextLevelBtn.classList.remove('btn-next');
+      this.pagination[this.level - 1].classList.remove('active');
+      this.pagination[this.level].classList.add('active');
+      this.birdImage.src = './assets/birdBlock.jpg';
+      this.questionText.textContent = '********';
+      this.answerInfo.innerHTML = `
+      <p style="text-align: center; font-size: 2vw;">Послушайте плеер. Выберите птицу из списка</p>
+    `;
+      this.audioInfo.pause();
+      this.audio.pause();
+      this.audio.currentTime = 0;
+      this.audioInfo.currentTime = 0;
+      this.playBtn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
+      this.generateAnswers();
+    }
+  }
+
+  showResult(){
+    const resultModal = customCreateElement('div', 'modal__container show-modal');
+    resultModal.innerHTML = `
+      <div class="modal__content">
+      <img class="modal__img" src="./assets/trophy.png" alt="trophy">
+        <div class="modal__close close-modal" title="Close">
+          <i class="bx bx-x"></i>
+        </div>
+        <h3 class="modal__title">Congratulations! Your score is ${this.score} out of 30</h3>
+        <input type="text" placeholder="Enter Your Name" class="reg" />
+        <div class="btn__container">
+          <button class="modal__button modal__button-width new">New Game</button>
+          <button class="modal__button modal__button-width save">Save</button>
+        </div>
+      </div>
+    `
+    this.gamePage.append(resultModal)
+    this.popup = document.querySelector('.modal__container');
+    this.closePopup = document.querySelector('.modal__close');
+    this.newGameBtn = document.querySelector('.new');
+    this.saveGameBtn = document.querySelector('.save');
+    this.closePopup.addEventListener('click', () => {
+      this.popup.classList.remove('show-modal');
+    });
+    this.newGameBtn.addEventListener('click', () => {
+      this.level = 0;
+      this.score = 0;
+      this.scoreCont.innerHTML = `Score: ${this.score}`;
+      this.container.innerHTML = '';
+      this.render()
+      this.afterRender()
+    });
+    this.saveGameBtn.addEventListener('click', () => {
+      const input = document.querySelector('.reg').value;
+      const res = {
+        name: input,
+        score: this.score
+      };
+      setToLocalStorage('score', JSON.stringify(res));
+    })
+  }
+  showModalResult() {
+    const modal = customCreateElement('div', 'modal__container show-modal');
+    modal.innerHTML = `
+      <div class="modal__content">
+        <div class="modal__close close-modal" title="Close">
+          <i class="bx bx-x"></i>
+        </div>
+        <h3 class="modal__title">Your score is ${this.score} out of 30</h3>
+        <button class="modal__button modal__button-width new">Try Again</button>
+      </div>
+    `;
+    this.gamePage.append(modal)
+    this.popup = document.querySelector('.modal__container');
+    this.closePopup = document.querySelector('.modal__close');
+    this.newGameBtn = document.querySelector('.new');
+    this.closePopup.addEventListener('click', () => {
+      this.popup.classList.remove('show-modal');
+    })
+    this.newGameBtn.addEventListener('click', () => {
+      this.level = 0;
+      this.score = 0;
+      this.scoreCont.innerHTML = `Score: ${this.score}`;
+      this.container.innerHTML = '';
+      this.render()
+      this.afterRender()
+    })
+  }
+
   render() {
     this.renderWrapper();
     return this.container;
@@ -261,5 +440,6 @@ export class GamePage extends Page {
     this.findElements();
     this.generateAnswers();
     this.listeners();
+    this.showResult()
   }
 }
